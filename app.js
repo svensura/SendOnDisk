241a34c22b24c674117d20a8edc3b4bf49dcb4c0
ssura <svensura@germanscreens.de>
2019-08-27 10:17:59 +0200

    First release

--- app.js
+++ app.js
@@ -15,11 +15,10 @@ const dir = appPath.sep + 'downloads' +  appPath.sep;
 const fullPath = process.execPath
 const lastIndex = fullPath.lastIndexOf(appPath.sep);
 
-outPath = fullPath.substring(0, lastIndex);
+const outPath = fullPath.substring(0, lastIndex);
 
 
 const appDir = args[1] || outPath; 
-//const appDir = args[1] || appPath.dirname(require.main.filename);
 const dnlDir = appDir + dir
 
 if (!fs.existsSync(dnlDir)){
@@ -30,13 +29,12 @@ const check_image = ((messages) => {
   messages.forEach((message) => {
     var id = message.id + '.png'
     try {
-      if (fs.existsSync(dnlDir + id)) {
-          message.photourl = `${dnlDir}${id}`
+      if (message.mediatype == 'photo' && fs.existsSync(dnlDir + id)) {
           console.log('File already exists under URL: ', message.photourl)
+          message.photourl = `${dnlDir}${id}`
         } else if (message.mediatype == 'photo') {
           download_image(message.photourl, dnlDir + id);
-          message.photourl = `${dnlDir}${id}`
-          console.log('File saved under new URL: ', message.photourl)
+          message.photourl = download_image(message.photourl, dnlDir + id);
         }
     } catch(err) {
       console.error(err)
@@ -49,35 +47,37 @@ const check_image = ((messages) => {
   
 
 const download_image = (url, filename) => {
-  axios({
-    url,
-    responseType: 'stream',
-  }).then(
-    response =>
-      new Promise((resolve, reject) => {
-        response.data
-          .pipe(fs.createWriteStream(filename))
-          .on('finish', () => resolve())
-          .on('error', e => reject(e));
-      }),
-)};
+  axios({ url, responseType: 'stream',})
+  .then(
+    response => new Promise((resolve, reject) => {
+      response.data
+        .pipe(fs.createWriteStream(filename))
+        .on('finish', () => {
+          resolve()
+          console.log('File saved under new URL: ', filename)
+          return filename
+        })
+        .on('error', e => reject(e));
+
+    }),
+  ).catch((e) => {
+    console.error(`Cannot write file ${filename} from ${url}!`);
+    return url
+  });
+};
+
+
 
 app.get('/:onScreenPath1/:onScreenPath2', async (req, res) => {
+  onScreenPath = req.params.onScreenPath1 + '/' + req.params.onScreenPath2
   try {
-    onScreenPath = req.params.onScreenPath1 + '/' + req.params.onScreenPath2
-    // console.log(onScreenPath)
     var newJson = await axios.get(`https://send.on-screen.info/api/${onScreenPath}`).then( resp => {
       var json = resp.data
-      //onsole.log('OLD_DATA', json)
       var oldMessages = json.project.messages
-      //console.log('OLD_MESSAGES', oldMessages)
       newMessages = check_image(oldMessages)
       json.project.messages = newMessages
       return json
     });
-
-    //data = JSON.stringify(newJson)
-
     res.status(200).send(newJson)
   } catch (e) {
     res.status(404).send()
